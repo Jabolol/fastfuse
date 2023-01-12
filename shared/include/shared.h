2b6fdc08f175711bb0c445d1521016f3e5b490e2
Javier Ríos <javier.rios-urbano@epitech.eu>
2023-01-12 20:05:40 +0100

    [update](shared/include/shared.h) Add `prototypes`, use `int32_t`

--- shared/include/shared.h
+++ shared/include/shared.h
@@ -1,7 +1,10 @@
 #ifndef __SHARED
     #define __SHARED
     #include <stdint.h>
-void swap_pointers(uint32_t **ptr_1, uint32_t **ptr_2);
-uint32_t split(uint32_t **array, uint32_t low, uint32_t high);
-void quicksort(uint32_t **array, uint32_t low, uint32_t high);
+void swap_pointers(int32_t **ptr_1, int32_t **ptr_2);
+int32_t split(int32_t **array, int32_t low, int32_t high);
+void quicksort(int32_t **array, int32_t low, int32_t high);
+void _swap_pointers(int32_t *ptr_1, int32_t *ptr_2);
+int32_t _split(int32_t *array, int32_t low, int32_t high);
+void _quicksort(int32_t *array, int32_t low, int32_t high);
 #endif
