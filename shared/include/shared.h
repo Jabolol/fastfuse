c7758b3a7a163668832d39a81788204f785f7dd7
Javier Ríos <javier.rios-urbano@epitech.eu>
2023-01-17 18:36:19 +0100

    [fix](shared/include/shared.h) Remove unused prototypes

--- shared/include/shared.h
+++ shared/include/shared.h
@@ -1,10 +1,19 @@
 #ifndef __SHARED
-    #define __SHARED
-    #include <stdint.h>
+#define __SHARED
+#include <stdint.h>
+typedef struct leaf leaf_t;
+
+typedef struct leaf {
+    leaf_t *left;
+    leaf_t *right;
+    int32_t count;
+    int32_t value;
+} leaf_t;
+
+#define MAX_ARR_LEN 256
+
 void swap_pointers(int32_t **ptr_1, int32_t **ptr_2);
 int32_t split(int32_t **array, int32_t low, int32_t high);
 void quicksort(int32_t **array, int32_t low, int32_t high);
-void _swap_pointers(int32_t *ptr_1, int32_t *ptr_2);
-int32_t _split(int32_t *array, int32_t low, int32_t high);
-void _quicksort(int32_t *array, int32_t low, int32_t high);
+void swap_pointers_leaves(leaf_t **ptr_1, leaf_t **ptr_2);
 #endif
