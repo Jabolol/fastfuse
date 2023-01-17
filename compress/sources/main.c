fec14682e239f5974c079f46a9367940b9095500
Javier Ríos <javier.rios-urbano@epitech.eu>
2023-01-17 18:33:28 +0100

    [fix](compress/sources/main.c) Take a different approach to implement the `Huffman` tree

--- compress/sources/main.c
+++ compress/sources/main.c
@@ -1,3 +1,4 @@
+
 #include <assert.h>
 #include <stdbool.h>
 #include <stddef.h>
@@ -5,39 +6,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 #include "../include/shared.h"
 #include "../include/my_printf.h"
 
-typedef struct leaf leaf_t;
+void min_heap(leaf_t **leaf_array, int32_t count, int32_t index)
+{
+    int32_t smallest = index;
+    int32_t left = 2 * index + 1;
+    int32_t rght = 2 * index + 2;
 
-typedef struct leaf {
-    leaf_t *left;
-    leaf_t *right;
-    int32_t count;
-    int32_t *value;
-} leaf_t;
+    if (left < count
+        && (*(leaf_array + left))->count < (*(leaf_array + smallest))->count) {
+        smallest = left;
+    }
+    if (rght < count
+        && (*(leaf_array + rght))->count < (*(leaf_array + smallest))->count) {
+        smallest = rght;
+    }
 
-#define MAX_ARR_LEN 256
+    if (smallest != index) {
+        swap_pointers_leaves(leaf_array + index, leaf_array + smallest);
+        min_heap(leaf_array, count, smallest);
+    }
+}
+
+void traverse_path(leaf_t *node, char **paths, char *buff, int32_t position)
+{
+    if (node->left == NULL && node->right == NULL) {
+        *(paths + node->value) = calloc(position + 1, 1);
+        strncpy(*(paths + node->value), buff, position);
+    } else {
+        *(buff + position) = '0';
+        traverse_path(node->left, paths, buff, position + 1);
+        *(buff + position) = '1';
+        traverse_path(node->right, paths, buff, position + 1);
+    }
+}
 
 int main(void)
 {
-    char *my_str =
-        "Albion Online es un mmorpg no lineal, en el que escribes tu propia "
-        "historia sin limitarte a seguir un camino prefijado. Explora un "
-        "amplio mundo abierto con 5 biomas únicos, todo cuánto hagas tendrá "
-        "su repercusión en el mundo, con la economía orientada al jugador de "
-        "Albion, los jugadores crean prácticamente todo el equipo a partir de "
-        "los recursos que consiguen, el equipo que llevas define quién eres, "
-        "cambia de arma y armadura para pasar de caballero a mago, o juega "
-        "como una mezcla de ambas clases. Aventúrate en el mundo abierto "
-        "frente a los habitantes y las criaturas de Albion, inicia "
-        "expediciones o adéntrate en mazmorras en las que encontrarás "
-        "enemigos aún más difíciles, enfréntate a otros jugadores en "
-        "encuentros en el mundo abierto, lucha por los territorios o por "
-        "ciudades enteras en batallas tácticas, relájate en tu isla privada, "
-        "donde podrás construir un hogar, cultivar cosechas y criar animales, "
-        "únete a un gremio, todo es mejor cuando se trabaja en grupo. "
-        "Adéntrate ya en el mundo de Albion y escribe tu propia historia.";
+    struct stat st;
+    if (lstat("./assets/pain.ppm", &st) != 0)
+        return 0;
+    char *bufff = malloc(st.st_size);
+    FILE *f = fopen("./assets/pain.ppm", "r");
+    fread(bufff, st.st_size, 1, f);
+    fclose(f);
+    char *my_str = bufff;
 
     int32_t string_length = strlen(my_str);
     int32_t **array_map = calloc(MAX_ARR_LEN, sizeof(int32_t *));
@@ -53,66 +69,95 @@ int main(void)
 
     quicksort(array_map, 0, MAX_ARR_LEN - 1);
 
-    int32_t *first_non_zero = NULL;
-    int32_t *last_ordered = pool + MAX_ARR_LEN - 1;
     int32_t new_length = MAX_ARR_LEN;
-    int32_t first_index = -1;
 
     for (int32_t i = 0; i < MAX_ARR_LEN; i++) {
         if (**(array_map + i) == 0) {
             new_length -= 1;
             continue;
         };
-        first_non_zero = *(array_map + i + 1);
-        first_index = i;
         break;
     }
 
-    /*
-    [
-          [0, 1, 2, 3, 4, 5]
-           [1, 2, 3, 4, 5]
-             [3, 3, 4, 5]
-              [6, 4, 5]
-                [9, 6]
-                 [15]
-    ]
-    */
-
     int32_t len = new_length;
-    int32_t **new_array = calloc(len * (len + 1) / 2, sizeof(int32_t *));
+    int32_t max_len = len * (len + 1) / 2;
+    leaf_t **leaf_array = calloc(max_len, sizeof(leaf_t *));
 
-    for (int32_t i = 0; i < new_length; i++, len--) {
-        if (i != 0)
-            _quicksort(*(new_array + i - 1), 0, len - 1);
-        *(new_array + i) = calloc(len, sizeof(int32_t));
-        if (i == 0) {
-            for (int z = first_index; z < MAX_ARR_LEN; z++) {
-                *(*(new_array + i) + (z - first_index)) = **(array_map + z);
-            }
-        }
-        for (int32_t j = 0; i != 0 && j < len; j++) {
-            if (j == 0) {
-                *(*(new_array + i) + j) =
-                    *(*(new_array + i - 1)) + *(*(new_array + i - 1) + 1);
-                continue;
-            }
-            *(*(new_array + i) + j) = *(*(new_array + i - 1) + j + 1);
+    int32_t leaf_count = 0;
+
+    for (int32_t i = 0; i < MAX_ARR_LEN; i++) {
+        if (*(pool + i) > 0) {
+            leaf_t *leaf = calloc(1, sizeof(leaf_t));
+            leaf->count = *(pool + i);
+            leaf->value = i;
+            *(leaf_array + leaf_count) = leaf;
+            leaf_count += 1;
         }
     }
 
-    int32_t aux_len = new_length;
-    for (int32_t i = 0; i < new_length; i++, aux_len--) {
-        fflush(stdout);
-        for (int32_t j = 0; j < aux_len; j++) {
-            printf("%d ", *(*(new_array + i) + j));
-        }
-        fprintf(stdout, "\n");
-        fflush(stdout);
+    for (int i = leaf_count / 2 - 1; i >= 0; i--) {
+        min_heap(leaf_array, leaf_count, i);
     }
 
-    free(pool);
+    leaf_t *root = NULL;
+
+    while (leaf_count > 1) {
+        leaf_t *left = *leaf_array;
+        *leaf_array = *(leaf_array + (leaf_count - 1));
+        leaf_count -= 1;
+        min_heap(leaf_array, leaf_count, 0);
+
+        leaf_t *right = *leaf_array;
+        *leaf_array = *(leaf_array + (leaf_count - 1));
+        leaf_count -= 1;
+        min_heap(leaf_array, leaf_count, 0);
+
+        root = calloc(1, sizeof(leaf_t));
+        root->count = left->count + right->count;
+        root->left = left;
+        root->right = right;
+
+        *(leaf_array + leaf_count) = root;
+        leaf_count += 1;
+        min_heap(leaf_array, leaf_count, leaf_count - 1);
+    }
+
+    char **paths = calloc(MAX_ARR_LEN, sizeof(char *));
+    char *buff = calloc(MAX_ARR_LEN, 1);
+
+    traverse_path(root, paths, buff, 0);
+
+    FILE *fstream = fopen("./out.bin", "wb");
+    if (!fstream)
+        return -1;
+
+    for (int j = 0; j < MAX_ARR_LEN; j++) {
+        if (*(paths + j) != NULL) {
+            printf("%s\n", *(paths + j));
+            uint8_t byte = 0;
+            int32_t bit_count = 0;
+
+            for (uint64_t i = 0; i < strlen(*(paths + j)); i++) {
+                byte = byte << 1;
+                if (*(*(paths + j) + i) == '1') {
+                    byte = byte | 1;
+                }
+                bit_count = bit_count + 1;
+                if (bit_count == 8) {
+                    fwrite(&byte, 1, 1, fstream);
+                    bit_count = 0;
+                    byte = 0;
+                }
+            }
+            if (bit_count > 0) {
+                byte = byte << (8 - bit_count);
+                fwrite(&byte, 1, 1, fstream);
+            }
+        }
+    }
+
+    fclose(fstream);
     free(array_map);
-    free(new_array);
-    return EXIT_SUCCESS;
+    free(pool);
+    free(bufff);
 }
