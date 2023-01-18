9e6be64ec92984f65edbbc8cbf12a4dd31402f8e
Javier Ríos <javier.rios-urbano@epitech.eu>
2023-01-18 11:30:56 +0100

    [fix](shared/include/shared.h) Fix coding style errors

--- shared/include/shared.h
+++ shared/include/shared.h
@@ -1,17 +1,22 @@
-#ifndef __SHARED
-#define __SHARED
-#include <stdint.h>
-typedef struct leaf leaf_t;
+/*
+** EPITECH PROJECT, 2022
+** shared.c
+** File description:
+** shared include file
+*/
 
+#ifndef __SHARED
+    #define __SHARED
+    #define MAX_ARR_LEN 256
+    #define BYTE_LEN    8
+    #include <stdint.h>
+typedef struct leaf leaf_t;
 typedef struct leaf {
     leaf_t *left;
     leaf_t *right;
     int32_t count;
     int32_t value;
 } leaf_t;
-
-#define MAX_ARR_LEN 256
-
 void swap_pointers(int32_t **ptr_1, int32_t **ptr_2);
 int32_t split(int32_t **array, int32_t low, int32_t high);
 void quicksort(int32_t **array, int32_t low, int32_t high);
