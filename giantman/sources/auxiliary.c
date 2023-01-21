c84ee20bf8e3518136c84c941d6f3b364a8a173c
Javier Ríos <javier.rios-urbano@epitech.eu>
2023-01-21 13:16:23 +0100

    [add](giantman/sources/files.c) Group functions by category

---
+++ giantman/sources/auxiliary.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2022
+** main.c
+** File description:
+** Giantman entry point
+*/
+
+#include "../include/shared.h"
+
+bool check_char(uint8_t const str)
+{
+    return str == SEPARATOR_BYTE;
+}
