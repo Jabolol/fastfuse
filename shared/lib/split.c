1178333b1dd2fd8417ed24f4285f515d78c8ee38
Javier Ríos <javier.rios-urbano@epitech.eu>
2023-01-21 13:21:54 +0100

    [fix](shared/lib/split.c) Use own `strdup`, `strlen` implementations, fix `coding style` errors

--- shared/lib/split.c
+++ shared/lib/split.c
@@ -9,8 +9,8 @@
 
 char **split_str(char const *readonly_str, bool (*fn)(uint8_t const))
 {
-    char *string = strdup(readonly_str);
-    uint32_t len = strlen(string);
+    char *string = my_strdup(readonly_str);
+    uint32_t len = my_strlen(string);
     uint32_t delim_len = 0;
     char **split_string = NULL;
     for (uint32_t i = 0; i < len; i++)
@@ -22,11 +22,10 @@ char **split_str(char const *readonly_str, bool (*fn)(uint8_t const))
         return NULL;
     if (!(split_string = my_calloc(delim_len + 2, sizeof(char **))))
         return NULL;
-    for (uint32_t i = 0; i < delim_len + 1;) {
-        uint32_t substr_len = strlen(string) + 1;
+    for (uint32_t i = 0; i < delim_len + 1; i++) {
+        uint32_t substr_len = my_strlen(string) + 1;
         *(split_string + i) = string;
         string = string + substr_len;
-        i = i + 1;
     }
     *(split_string + delim_len + 1) = NULL;
     return split_string;
