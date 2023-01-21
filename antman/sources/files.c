137df175f247014d282bd5a941de1e32eb7b4ecb
Javier Ríos <javier.rios-urbano@epitech.eu>
2023-01-21 13:15:31 +0100

    [add](antman/sources/files.c) Group functions by category

---
+++ antman/sources/files.c
@@ -0,0 +1,78 @@
+/*
+** EPITECH PROJECT, 2022
+** main.c
+** File description:
+** Antman entry point
+*/
+
+#include "../include/antman.h"
+#include "../include/shared.h"
+#include "../include/my_printf.h"
+
+void write_header(char **paths)
+{
+    uint8_t temp = 0;
+
+    for (int32_t i = 0; i < MAX_ARR_LEN; i++) {
+        if (*(paths + i) != NULL) {
+            temp = i;
+            fwrite(&temp, 1, 1, stdout);
+            uint64_t len = strlen(*(paths + i));
+            fwrite(*(paths + i), 1, len, stdout);
+            temp = SEPARATOR_BYTE;
+            fwrite(&temp, 1, 1, stdout);
+        }
+    }
+    temp = END_BYTE;
+    fwrite(&temp, 1, 1, stdout);
+}
+
+void process_byte(char path_bit, uint8_t *byte, int32_t *bit_count)
+{
+    if (path_bit == '1')
+        *byte |= 1;
+    *bit_count += 1;
+    if (*bit_count == BYTE_LEN) {
+        fwrite(byte, 1, 1, stdout);
+        *bit_count = 0;
+        *byte = 0;
+    }
+}
+
+void process_path(
+    char **paths, int32_t count, uint8_t *byte, int32_t *bit_count)
+{
+    if (*(paths + count) != NULL) {
+        uint64_t len = my_strlen(*(paths + count));
+        for (uint64_t i = 0; i < len; i++, *byte <<= 1)
+            process_byte(*(*(paths + count) + i), byte, bit_count);
+    }
+}
+
+void write_payload(char **paths, uint8_t *my_str, struct stat *st)
+{
+    uint8_t byte = 0;
+    int32_t bit_count = 0;
+    for (int32_t j = 0; j < st->st_size; j++)
+        process_path(paths, *(my_str + j), &byte, &bit_count);
+    if (bit_count > 0) {
+        byte <<= (BYTE_LEN - 1 - bit_count);
+        fwrite(&byte, 1, 1, stdout);
+    }
+}
+
+int32_t write_data(leaf_t *root, uint8_t *my_str, struct stat *st)
+{
+    char **paths = my_calloc(MAX_ARR_LEN, sizeof(char *));
+    char *buff = my_calloc(MAX_ARR_LEN, 1);
+    if (paths == NULL || buff == NULL) {
+        my_printf("Couldn't allocate memory, aborting...\n");
+        return EXIT_ERROR;
+    }
+    traverse_path(root, paths, buff, 0);
+    write_header(paths);
+    write_payload(paths, my_str, st);
+    free(paths);
+    free(buff);
+    return 0;
+}
