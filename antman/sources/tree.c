61408d6480aa4ab58e3ecddde85734e334364184
Javier Ríos <javier.rios-urbano@epitech.eu>
2023-01-21 13:16:14 +0100

    [add](antman/sources/tree.c) Group functions by category

---
+++ antman/sources/tree.c
@@ -0,0 +1,89 @@
+/*
+** EPITECH PROJECT, 2022
+** main.c
+** File description:
+** Antman entry point
+*/
+
+#include "../include/shared.h"
+
+void min_heap(leaf_t **leaf_array, int32_t count, int32_t index)
+{
+    int32_t smallest = index;
+    int32_t left = 2 * index + 1;
+    int32_t rght = 2 * index + 2;
+
+    if (left < count
+        && (*(leaf_array + left))->count < (*(leaf_array + smallest))->count) {
+        smallest = left;
+    }
+    if (rght < count
+        && (*(leaf_array + rght))->count < (*(leaf_array + smallest))->count) {
+        smallest = rght;
+    }
+
+    if (smallest != index) {
+        swap_pointers_leaves(leaf_array + index, leaf_array + smallest);
+        min_heap(leaf_array, count, smallest);
+    }
+}
+
+void traverse_path(leaf_t *node, char **paths, char *buff, int32_t position)
+{
+    if (node->left == NULL && node->right == NULL) {
+        *(paths + node->value) = my_calloc(position + 1, 1);
+        my_strncpy(*(paths + node->value), buff, position);
+    } else {
+        *(buff + position) = '0';
+        traverse_path(node->left, paths, buff, position + 1);
+        *(buff + position) = '1';
+        traverse_path(node->right, paths, buff, position + 1);
+    }
+}
+
+leaf_t **populate_leaves(
+    int32_t new_length, int32_t *pool, int32_t *leaf_count)
+{
+    int32_t len = new_length;
+    int32_t max_len = len * (len + 1) / 2;
+    leaf_t **leaf_array = my_calloc(max_len, sizeof(leaf_t *));
+
+    for (int32_t i = 0; i < MAX_ARR_LEN; i++) {
+        if (*(pool + i) > 0) {
+            leaf_t *leaf = my_calloc(1, sizeof(leaf_t));
+            leaf->count = *(pool + i);
+            leaf->value = i;
+            *(leaf_array + *leaf_count) = leaf;
+            *leaf_count += 1;
+        }
+    }
+
+    for (int32_t i = *leaf_count / 2 - 1; i >= 0; i--) {
+        min_heap(leaf_array, *leaf_count, i);
+    }
+    return leaf_array;
+}
+
+void build_tree(leaf_t **root, leaf_t **leaf_array, int32_t leaf_count)
+{
+    for (; leaf_count > 1;) {
+        leaf_t *left = *leaf_array;
+        *leaf_array = *(leaf_array + (leaf_count - 1));
+        leaf_count -= 1;
+        min_heap(leaf_array, leaf_count, 0);
+
+        leaf_t *right = *leaf_array;
+        *leaf_array = *(leaf_array + (leaf_count - 1));
+        leaf_count -= 1;
+        min_heap(leaf_array, leaf_count, 0);
+
+        *root = my_calloc(1, sizeof(leaf_t));
+        (*root)->count = left->count + right->count;
+        (*root)->left = left;
+        (*root)->right = right;
+
+        *(leaf_array + leaf_count) = *root;
+        leaf_count += 1;
+        min_heap(leaf_array, leaf_count, leaf_count - 1);
+    }
+}
