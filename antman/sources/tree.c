fa4c644e5400c493552e943ed5ded03894bb84ef
Javier Ríos <javier.rios-urbano@epitech.eu>
2023-01-22 20:18:25 +0100

    [fix](antman/sources/tree.c) Add even more `calloc` checks

--- antman/sources/tree.c
+++ antman/sources/tree.c
@@ -32,6 +32,8 @@ void traverse_path(leaf_t *node, char **paths, char *buff, int32_t position)
 {
     if (node->left == NULL && node->right == NULL) {
         *(paths + node->value) = my_calloc(position + 1, 1);
+        if (*(paths + node->value) == NULL)
+            return;
         my_strncpy(*(paths + node->value), buff, position);
     } else {
         *(buff + position) = '0';
@@ -47,17 +49,19 @@ leaf_t **populate_leaves(
     int32_t len = new_length;
     int32_t max_len = len * (len + 1) / 2;
     leaf_t **leaf_array = my_calloc(max_len, sizeof(leaf_t *));
-
+    if (!leaf_array)
+        return NULL;
     for (int32_t i = 0; i < MAX_ARR_LEN; i++) {
-        if (*(pool + i) > 0) {
-            leaf_t *leaf = my_calloc(1, sizeof(leaf_t));
-            leaf->count = *(pool + i);
-            leaf->value = i;
-            *(leaf_array + *leaf_count) = leaf;
-            *leaf_count += 1;
-        }
+        if (!(*(pool + i) > 0))
+            continue;
+        leaf_t *leaf = my_calloc(1, sizeof(leaf_t));
+        if (!leaf)
+            return NULL;
+        leaf->count = *(pool + i);
+        leaf->value = i;
+        *(leaf_array + *leaf_count) = leaf;
+        *leaf_count += 1;
     }
-
     for (int32_t i = *leaf_count / 2 - 1; i >= 0; i--) {
         min_heap(leaf_array, *leaf_count, i);
     }
@@ -66,22 +70,22 @@ leaf_t **populate_leaves(
 
 void build_tree(leaf_t **root, leaf_t **leaf_array, int32_t leaf_count)
 {
+    if (root == NULL || leaf_array == NULL)
+        return;
     for (; leaf_count > 1;) {
         leaf_t *left = *leaf_array;
         *leaf_array = *(leaf_array + (leaf_count - 1));
         leaf_count -= 1;
         min_heap(leaf_array, leaf_count, 0);
-
         leaf_t *right = *leaf_array;
         *leaf_array = *(leaf_array + (leaf_count - 1));
         leaf_count -= 1;
         min_heap(leaf_array, leaf_count, 0);
-
-        *root = my_calloc(1, sizeof(leaf_t));
+        if (!(*root = my_calloc(1, sizeof(leaf_t))))
+            return;
         (*root)->count = left->count + right->count;
         (*root)->left = left;
         (*root)->right = right;
-
         *(leaf_array + leaf_count) = *root;
         leaf_count += 1;
         min_heap(leaf_array, leaf_count, leaf_count - 1);
